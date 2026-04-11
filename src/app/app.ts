0876ea6602b97a2966eb26928773ec46bb12afa5
RodolphoAntunes7 <rodolpho10_antunes@hotmail.com>
2026-04-11 14:41:24 -0300

    Componente historia e localizacao

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,5 @@
 import { Component, signal } from '@angular/core';
 import { DressCode } from './sections/dress-code/dress-code';
-import { Galeria } from './sections/galeria/galeria';
 import { Localizacao } from './sections/localizacao/localizacao';
 import { HeroComponent } from './sections/hero/hero';
 import { NossaHistoria } from './sections/nossa-historia/nossa-historia';
@@ -10,7 +9,7 @@ import { StickyNav } from './components/sticky-nav/sticky-nav';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [DressCode, Galeria, HeroComponent, Localizacao, NossaHistoria, Rsvp, StickyNav],
+  imports: [DressCode, HeroComponent, Localizacao, NossaHistoria, Rsvp, StickyNav],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
